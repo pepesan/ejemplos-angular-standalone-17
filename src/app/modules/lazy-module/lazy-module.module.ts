84e3bad6c43b732861c4a63ad0c2daece7815b76
David Vaquero <pepesan@gmail.com>
2024-01-25 13:50:08 +0100

    metido componente para carga de sub ruta del módulo

--- src/app/modules/lazy-module/lazy-module.module.ts
+++ src/app/modules/lazy-module/lazy-module.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MyLazyComponent} from "./components/my-lazy/my-lazy.component";
 import {RouterModule, Routes} from "@angular/router";
+import {DemoComponent} from "./components/demo/demo.component";
 
 const routes: Routes = [
   { path: '', component: MyLazyComponent },
+  { path: 'demo', component: DemoComponent },
 ];
 
 @NgModule({
-  declarations: [MyLazyComponent],
+  declarations: [MyLazyComponent, DemoComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
