74b64e792fe91cf5b9d51c59cb90ff0e01e06673
pepesan <pepesan@gmail.com>
2024-07-04 12:04:06 +0200

    mejora del ejemplo basico de formulario para meter un value en cada option y modificado el listado de posibles poderes para incluir un id para poder iterar con @for

--- src/app/components/formularios/main-form/hero.ts
+++ src/app/components/formularios/main-form/hero.ts
@@ -2,7 +2,7 @@ export class Hero {
   constructor(
     public id: number = 0,
     public name: string = "",
-    public power: string = "",
+    public power: number = 0,
     public alterEgo?: string
   ) {  }
 }
--- src/app/components/formularios/main-form/main-form.component.ts
+++ src/app/components/formularios/main-form/main-form.component.ts
@@ -16,15 +16,31 @@ import {FormsModule} from "@angular/forms";
 export class MainFormComponent implements OnInit {
   public model: Hero;
   public showedModel: Hero;
-  public powers: string[] =  [
-    'Really Smart',
-    'Super Flexible',
-    'Super Hot',
-    'Weather Changer'];
+  public powers: any[] =  [
+    {
+      id: 0,
+      text: 'Really Smart'
+    },
+    {
+      id: 1,
+      text: 'Super Flexible'
+    },
+    {
+      id: 2,
+      text: 'Super Hot'
+    },
+    {
+      id: 3,
+      text: 'Weather Changer'
+    }];
   heroes: Hero[] = [];
 
   constructor() {
-    this.model = new Hero(0,"",this.powers[0],"");
+    this.model = new Hero(
+      0,
+      "",
+      0, // por defecto es Really Smart
+      "");
     this.showedModel = new Hero();
   }
 
