4de309630480e79dd6911c1fdc9107fc5c5770b8
David Vaquero <pepesan@gmail.com>
2025-04-09 12:59:45 +0200

    mejoras del ejemplo de señales

--- src/app/components/signals/signals.component.ts
+++ src/app/components/signals/signals.component.ts
@@ -1,29 +1,38 @@
 import {Component, computed, OnInit, Signal, signal, WritableSignal} from '@angular/core';
-import {NgForOf} from "@angular/common";
+import {JsonPipe, NgForOf} from "@angular/common";
+import {Dato} from "../propiedades/dato";
 
 @Component({
     selector: 'app-signals',
-    imports: [
-        NgForOf
-    ],
+  imports: [
+    NgForOf,
+    JsonPipe
+  ],
     templateUrl: './signals.component.html',
     styleUrls: ['./signals.component.sass']
 })
 export class SignalsComponent implements OnInit{
-  // definición de una variable string con signals
+  // defino una señal del tipo number
+  senalNumero: WritableSignal<number> = signal(0);
+  // defino una señal del tipo string
+  senalCadena: WritableSignal<string> = signal("Valor");
+  // definimos un signal que es un string[]
+  cadenasSignal: WritableSignal<string[]> = signal([]);
+  // defino una señal del tipo custom
+  senalDato: WritableSignal<Dato> = signal(new Dato());
+  // definición de una variable number de un contador con signals
   countSignal: WritableSignal<number> = signal(0);
   // Computed permite subscribirse al cambio de otro signal y aplicar una función que cambie el valor de la variable computada
   // cada vez que cambie count cambiará doubleCount, aplicando una función para el cálculo
   doubleCount: Signal<number> = computed(() => this.countSignal() * 2);
-  // definimos un signal que es un string[]
-  cadenasSignal: WritableSignal<string[]> = signal([]);
+
   // Signals are getter functions - calling them reads their value.
 
   ngOnInit(): void {
     // cambiar un valor por otro
     this.countSignal.set(2)
     // Increment the count by 1.
-    // Cambiar el valor usando un a función
+    // Cambiar el valor usando un a función dependiendo del valor actual del signal
     this.countSignal.update((value: number) => value + 1);
     // Mutate cambia el valor interno sin reescribir su contenido (fallo en v17)
     // this.cadenasSignal.mutate((value: string[]): void => {
