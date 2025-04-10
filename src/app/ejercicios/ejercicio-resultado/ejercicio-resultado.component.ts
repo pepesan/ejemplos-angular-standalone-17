7dd7162a130bd221f8f9d42c69d5eaaa6d8e0984
David Vaquero <pepesan@gmail.com>
2025-04-10 12:13:39 +0200

    resuelto ejercicio básico de servicios http de maner básica

--- src/app/ejercicios/ejercicio-resultado/ejercicio-resultado.component.ts
+++ src/app/ejercicios/ejercicio-resultado/ejercicio-resultado.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import {Resultado} from "./resultado";
 import {ResultadoService} from "./resultado.service";
 import {AsyncPipe, NgForOf} from "@angular/common";
@@ -16,9 +16,22 @@ export class EjercicioResultadoComponent {
 
   public listado: Promise<Resultado[]> ;
 
+  // Uso con señales
+  public partidosSignal: WritableSignal<Resultado[]> = signal<Resultado[]>([]);
+
   constructor(private resultadoService: ResultadoService) {
     // Directamente consultamos al servicio para que devuelva una promesa con los datos del JSON
     this.listado = this.resultadoService.getData().toPromise();
+
+    // Uso con señales
+    this.resultadoService.getData().subscribe(
+      // Define una función que se ejecutará cuando se disponga del dato
+      // data es el dato recibido
+      data =>{
+        // cargar en la señal el dato descargado
+        this.partidosSignal.set(data);
+      }
+    );
   }
 
 
