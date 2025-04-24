5a76dd616b2d6e4cf6a8ee2f48a04a3e1a10dfa5
David Vaquero <pepesan@gmail.com>
2025-04-24 11:32:20 +0200

    mejora del ejemplo del peticion post con unsubscribe

--- src/app/components/peticion-post/peticion-post.component.ts
+++ src/app/components/peticion-post/peticion-post.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {PostData} from "../../classes/post-data";
 import {PeticionPostService} from "../../services/peticion-post.service";
 import {FormsModule} from "@angular/forms";
 import {JsonPipe, NgIf} from "@angular/common";
 import {PostDataResponse} from "../../classes/post-data-response";
-import {firstValueFrom} from "rxjs";
+import {Subscription} from "rxjs";
 
 
 @Component({
@@ -17,7 +17,7 @@ import {firstValueFrom} from "rxjs";
   templateUrl: './peticion-post.component.html',
   styleUrl: './peticion-post.component.css'
 })
-export class PeticionPostComponent {
+export class PeticionPostComponent implements OnDestroy{
 
   postData: PostData = {
     title: '',
@@ -28,18 +28,27 @@ export class PeticionPostComponent {
   response: PostDataResponse | null = null;
   error: string | null = null;
 
+  public datoObservableSubscription: Subscription | null = null;
+
   constructor(private postService: PeticionPostService) {
+
   }
 
   submitPost() {
     this.error = null;
     try {
-      this.postService.sendPost(this.postData).subscribe(data => {
+      this.datoObservableSubscription=this.postService.sendPost(this.postData).subscribe(data => {
         this.response = data;
         console.log('Respuesta del servidor:', data);
       });
     } catch (err: any) {
       this.error = err.message || 'Error desconocido al enviar el post';
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.datoObservableSubscription) {
+      this.datoObservableSubscription.unsubscribe();
     }
   }
 
