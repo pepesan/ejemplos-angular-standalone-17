5d33620c6e106ca3e6cc912f9d35e225c524df64
David Vaquero <pepesan@gmail.com>
2025-04-24 11:38:20 +0200

    mejora del ejemplo de ciclo de vida de componentes

--- src/app/components/ciclo-de-vida/ciclo-de-vida.component.ts
+++ src/app/components/ciclo-de-vida/ciclo-de-vida.component.ts
@@ -1,26 +1,54 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked, OnDestroy, SimpleChanges } from '@angular/core';
 
 @Component({
-  selector: 'app-ciclo-de-vida',
+  selector: 'app-ciclo-vida',
   imports: [],
   templateUrl: './ciclo-de-vida.component.html',
-  styleUrl: './ciclo-de-vida.component.css'
+  styleUrl: './ciclo-de-vida.component.css',
 })
-export class CicloDeVidaComponent implements OnInit, OnDestroy{
+export class CicloDeVidaComponent implements OnInit, OnChanges, DoCheck,
+  AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
-  public propiedad: string;
+  dato: string = 'Hola mundo';
 
   constructor() {
-    console.log('Ciclo de vida: constructor');
-    this.propiedad = 'partidos';
+    console.log('Constructor: se ha creado el componente');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges: cambios detectados en @Input', changes);
   }
 
   ngOnInit(): void {
-    console.log('Ciclo de vida: ngOnInit');
+    console.log('ngOnInit: inicialización del componente');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck: detección manual de cambios');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit: contenido proyectado inicializado');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked: contenido proyectado verificado');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit: vista del componente inicializada');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked: vista del componente verificada');
   }
 
   ngOnDestroy(): void {
-    console.log('Ciclo de vida: ngOnDestroy');
+    console.log('ngOnDestroy: limpieza antes de destruir el componente');
   }
-
+  cambia(){
+    this.dato = 'Hola mundo cambiado';
+    console.log('Cambiando dato');
+  }
 }
