0af95d13dcaa719de46cb36c31866be8f1143c0f
David Vaquero <pepesan@gmail.com>
2025-04-25 13:20:13 +0200

    mejora de ejemplos de rutas lazy

--- src/app/services/listado.service.ts
+++ src/app/services/listado.service.ts
@@ -7,7 +7,7 @@ export class ListadoService {
   items: any[]
   constructor() {
     this.items = [
-      { id: 1, name: 'Item 1' },
+        { id: 1, name: 'Item 1' },
       { id: 2, name: 'Item 2' },
       { id: 3, name: 'Item 3' }
     ];
